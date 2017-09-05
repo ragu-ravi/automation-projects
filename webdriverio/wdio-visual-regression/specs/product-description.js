bf4de1f05cd9b9ba69eb32a3cbaaccb60d2edad8
Ragupathi Ravi <ragupathi.ravi@in.tesco.com>
2017-09-05 06:32:49 +0100

    wdio-visual-regression: fix multiple different browser width issue

--- webdriverio/wdio-visual-regression/specs/product-description.js
+++ webdriverio/wdio-visual-regression/specs/product-description.js
@@ -6,13 +6,13 @@ const compareImages = outputs => {
 
 describe('Feature: Product Description Page', () => {
   context('Product container check', () => {
-    const productId = '254656543'
+    const productId = '253807413'
     before(() => {
       PDP.open(productId);
     });
 
     it('product container elements should be aligned properly', () => {
-      compareImages(browser.checkElement(PDP.rest_of_shelf.selector));
+      compareImages(browser.checkElement(PDP.pdp_container.selector));
     });
   });
 });
