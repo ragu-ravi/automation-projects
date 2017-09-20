ccdd3342877014e5fadef60f3a27082808c3768d
Girish Pottigari <girishpottigari@Girish-off-MacBook-Pro-2.local>
2017-09-20 14:21:31 +0100

    converting section class into a method

--- webdriverio/wdio-mocha/page-objects/helpers/sections.js
+++ webdriverio/wdio-mocha/page-objects/helpers/sections.js
@@ -1,20 +1,12 @@
-class Section {
-  constructor(name,css) {
-    this.section = name
-    this.css = css
-  }
-
-  items() {
-    for(var results=[],i=1,size=$$(this.css).length;i <= size; i++) {
-      results.push(new this.section(this.css,i));
+function sections(name,css) {
+    for(var results=[],i=1,size=$$(css).length;i <= size; i++) {
+      results.push(new name(css,i));
     }
     return results;
-  }
-
-  item() {
-    return new this.section(this.css,1)
-  }
-
 }
 
-module.exports = Section;
+function section(name,css) {
+    return new name(css,1);
+}
+
+module.exports = { sections,section};
--- webdriverio/wdio-mocha/page-objects/pages/desktop/search-results-page.js
+++ webdriverio/wdio-mocha/page-objects/pages/desktop/search-results-page.js
@@ -1,12 +1,12 @@
 import Page from '../../page';
 import Product from './sections/products_section'
-import Section from '../../helpers/sections'
+import {sections,section} from '../../helpers/sections'
 
 class SearchResults extends Page {
   get search_heading() { return $('h1.heading'); }
   get product_list() { return $$('.product-list--list-item'); }
-  get products() { return new Section(Product,'.product-list--list-item').items() }
-  get product() { return new Section(Product,'.product-list--list-item').item()}
+  get products() { return sections(Product,'.product-list--list-item') }
+  get product() { return section(Product,'.product-list--list-item') }
 
 
   get_search_heading() {
