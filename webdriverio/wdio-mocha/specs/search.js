9277e411ac5870c5e783bdafba31792acb83c6cc
Ragu <ragu4584@gmail.com>
2017-09-22 11:36:28 +0100

    Update search.js

--- webdriverio/wdio-mocha/specs/search.js
+++ webdriverio/wdio-mocha/specs/search.js
@@ -16,7 +16,7 @@ describe('Feature: Search', () => {
     it('product list should not be empty', () => {
       expect(app.search_page.products).to.not.have.lengthOf(0);
       console.log('Title of 1st product :', app.search_page.product.title.getText());
-      console.log('Title of 2nd product :', app.search_page.products[2].title.getText());
+      console.log('Title of 2nd product :', app.search_page.products[1].title.getText());
       expect(app.search_page.products[0].title.getText()).to.include(query);
     });
   });
